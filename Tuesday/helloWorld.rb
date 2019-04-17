1af4f280cca37c00e6a024765919f0a76c6c0009
Parth Sharma <parth.sharma@trios.com>
2019-04-16 18:17:09 -0700

    Tuesday

---
+++ Tuesday/helloWorld.rb
@@ -0,0 +1,34 @@
+#puts "hello world !"
+
+puts "I am learning Ruby !"
+
+p "I am enjoying ruby !"
+
+p " What a wondsddg  fghd"
+
+age = 10  ;first_name = "John"
+
+PI = 3.14
+VALUE_OF_PI = 3.14
+
+CamelCase
+
+# the correct syntax shoud be age == 10
+if age=100
+	puts "Your age is 10"
+else
+	puts "WE don't know your age"
+end	
+
+if age != 6
+	puts "Your age is not 6"
+end	
+
+unless age == 6
+	puts "Your age is not 6"
+end	
+
+
+for i in 0..9
+	puts "Hello Wiorld !"
+end
