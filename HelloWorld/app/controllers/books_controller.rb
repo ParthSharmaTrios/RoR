c7b020202bdd3e543586e67b56b81b70cd0cf86e
Parth Sharma <parth.sharma@trios.com>
2019-05-02 18:16:20 -0700

    Insert | Select | bootstrap

--- HelloWorld/app/controllers/books_controller.rb
+++ HelloWorld/app/controllers/books_controller.rb
@@ -1,11 +1,30 @@
 class BooksController < ApplicationController
 
   def show
-
+      @list_of_books = Book.all
   end
 
   def new
     @nb = Book.new
+  end
+
+  def create
+
+    @book = Book.new(
+            params.require(:book).permit(:title, :author, :description)
+    )
+
+    result = @book.save()
+
+    if result
+      redirect_to :controller => 'books' , :action => 'show'
+    else
+      redirect_to_back
+    end
+
+
+
+
   end
 
 
--- HelloWorld/db/migrate/20190501235705_create_books.rb
+++ HelloWorld/db/migrate/20190501235705_create_books.rb
@@ -2,7 +2,7 @@ class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
       t.string :title
-      t.text :description
+      t .text :description
       t.string :author
 
       t.timestamps
